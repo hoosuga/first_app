c043c8db25864064bcb1f4b7c8266b893e15e9b3
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-04-15 10:45:03 +0000

    [Update] 2-5 カラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_15_104125) do
+ActiveRecord::Schema.define(version: 2023_04_15_104411) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
   end
 
 end
